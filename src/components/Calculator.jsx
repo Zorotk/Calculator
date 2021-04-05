99582bae4214974bd8a8582dd1da4781a2f86888
Blazor <zorotkov@rambler.ru>
2021-04-05 20:19:41 +0300

    calc

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,45 +1,46 @@
 import React, { useState } from 'react';
 import '../styles/toolbar.scss'
 
-
+const btnValues = '1234567890=-+/*'.split('')
 const Calculator = () => {
     const [display, setdisplay] = useState([])
     const [currentOperator, setcurrentOperator] = useState('')
     const [value1, setvalue1] = useState('')
     const [value2, setvalue2] = useState('')
     const [result, setresult] = useState()
-    const btnValues = '1234567890=-+/*'.split('')
 
-    function test() {
-        let v1 = Number(value1.slice(0, -1))
-        let v2 = Number(value2)
-        
-        return v1 + v2 
+
+    function calc(v1, v2, c) {
+        switch (c) {
+            case '+': return v1 + v2
+            case '-': return v1 - v2
+            case '*': return v1 * v2
+            case '/': return v1 / v2
+        }
     }
 
     const btnHandler = (btn) => {
-        if (value2 === '' && currentOperator == '') {
-            setvalue1(value1 + btn)
-        }
-        console.log(currentOperator)
-        if (btnValues.slice(10).some(v => v == btn)) {
-            setcurrentOperator(btn)
-        }
-        if (currentOperator !== '') {
-            setvalue2(value2 + btn)
-            if (btnValues.slice(10).some(v => v == btn)) {
-                setvalue1('')
-                setvalue2('')
-                setcurrentOperator('')
-                setresult(test())                   
-            }
-        }
         display.push(btn)
         setdisplay([...display])
+        const isOperator = btnValues.slice(10).some(v => v == btn)
+        if (isOperator) {
+            if (value2 !== '' && currentOperator) {
+                setvalue1(calc(+value1, +value2, currentOperator))
+                setvalue2('')
+                display.push(calc(+value1, +value2, currentOperator))
+                setdisplay([...display])
+            }
+            setcurrentOperator(btn === '=' ? '' : btn)
+        } else {
+            if (currentOperator) {
+                setvalue2(value2 + btn)
+            } else {
+                setvalue1(value1 + btn)
+            }
+        }
     }
-    console.log(display.map(el => {
-        return isNaN(el) ? el : +el
-    }))
+
+
     return (
         <div className="Calculator">
             <div className={'Calculator__panel'}>
