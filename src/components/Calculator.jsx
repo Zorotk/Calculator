f5fbe24d1be308dfce2daeb39e86b6c4585fb39e
Blazor <zorotkov@rambler.ru>
2021-04-06 13:33:50 +0300

    add zero

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -8,11 +8,11 @@ const Calculator = observer(() => {
         <div className="Calculator">
             {/* <h2>{CalcState.display.join('')}</h2> */}
             <div className={'Calculator__panel'}>
-                <h2> {CalcState.value2 == '' ? CalcState.value1 : CalcState.value2}</h2>
+                <h2> {CalcState.value2 == '' ? CalcState.value1 == '' ? 0 : CalcState.value1 : CalcState.value2}</h2>
                 <Button variant="success" className={'Calculator__panel__ext'} onClick={() => CalcState.extend()}>расширенный режим</Button>
                 <div >
                     {CalcState.btnValues.map((btn, i) => (
-                        <Button variant="light" className="Calculator__panel__btn" onClick={() => CalcState.btnHandler(btn)} key={i}>{btn} </Button>
+                        <Button variant={"light"} className="Calculator__panel__btn" onClick={() => CalcState.btnHandler(btn)} key={i}>{btn} </Button>
                     ))}
                 </div>
             </div>
