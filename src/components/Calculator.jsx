265092da0f79b9d31830d6abd5556eb7157e8af2
Blazor <zorotkov@rambler.ru>
2021-04-06 11:36:10 +0300

    add mobX

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,62 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../styles/toolbar.scss'
+import CalcState from '../store/calcState'
+import { observer } from 'mobx-react-lite'
 
-const btnValues = '1234567890=-+/*'.split('')
-const Calculator = () => {
-    const [display, setdisplay] = useState([])
-    const [currentOperator, setcurrentOperator] = useState('')
-    const [value1, setvalue1] = useState('')
-    const [value2, setvalue2] = useState('')
-    const [result, setresult] = useState()
-
-
-    function calc(v1, v2, c) {
-        switch (c) {
-            case '+': return v1 + v2
-            case '-': return v1 - v2
-            case '*': return v1 * v2
-            case '/': return v1 / v2
-        }
-    }
-
-    const btnHandler = (btn) => {
-        display.push(btn)
-        setdisplay([...display])
-        const isOperator = btnValues.slice(10).some(v => v == btn)
-        if (isOperator) {
-            if (value2 !== '' && currentOperator) {
-                setvalue1(calc(+value1, +value2, currentOperator))
-                setvalue2('')
-                display.push(calc(+value1, +value2, currentOperator))
-                setdisplay([...display])
-            }
-            setcurrentOperator(btn === '=' ? '' : btn)
-        } else {
-            if (currentOperator) {
-                setvalue2(value2 + btn)
-            } else {
-                setvalue1(value1 + btn)
-            }
-        }
-    }
-
-
+const Calculator = observer(() => {
     return (
         <div className="Calculator">
+            {/* <h2>{CalcState.display.join('')}</h2> */}
             <div className={'Calculator__panel'}>
-                <h2>{display.join('')}</h2>
-                <h2>znak{currentOperator}</h2>
-                <h2>1 {value1}</h2>
-                <h2>2 {value2}</h2>
-                <h2>resul {result}</h2>
-                <div className="Calculator__panel__btn">
-                    {btnValues.map((btn, i) => (
-                        <button onClick={() => btnHandler(btn)} key={i}>{btn}</button>
+                <h2> {CalcState.value2 == '' ? CalcState.value1 : CalcState.value2}</h2>
+                <button onClick={() => CalcState.extend()}>расширенный режим</button>
+                <div >
+                    {CalcState.btnValues.map((btn, i) => (
+                        <button className="Calculator__panel__btn" onClick={() => CalcState.btnHandler(btn)} key={i}>{btn} </button>
                     ))}
                 </div>
             </div>
         </div>
     );
-};
+}
+)
 
 export default Calculator;
---
+++ src/store/calcState.js
@@ -0,0 +1,59 @@
+import { makeAutoObservable } from "mobx";
+
+class CalcState {
+    btnValues = '1234567890=-+/*←c'.split('')
+    value1 = ''
+    value2 = ''
+    currentOperator = ''
+    display = []
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    extend() {
+        const extArr = ['x*', 'sqrt']
+        this.btnValues.length === 17
+            ? this.btnValues.push(...extArr)
+            : this.btnValues.splice((this.btnValues.length - extArr.length), extArr.length)
+    }
+
+    calc(v1, v2, c) {
+        switch (c) {
+            case '+': return v1 + v2
+            case '-': return v1 - v2
+            case '*': return v1 * v2
+            case '/': return v1 / v2
+            case 'c': return this.value1 = '', this.value2 = '', this.display = []
+            case '←': return this.currentOperator
+                ? this.value2 = this.value2.substring(0, this.value2.length - 1)
+                : this.value1 = this.value1.substring(0, this.value1.length - 1)
+            case 'x*': return v1 ** v2
+            case 'sqrt': return this.value1 = Math.sqrt(v1)
+        }
+    }
+
+    btnHandler = (btn) => {
+        if (btn == 'sqrt' || btn == '←' || btn == 'c') {
+            return this.calc(+this.value1, +this.value2, btn)
+        }
+        this.display.push(btn)
+
+        const isOperator = this.btnValues.slice(10).some(v => v == btn)
+        if (isOperator) {
+            if (this.value2 !== '' && this.currentOperator) {
+                this.value1 = this.calc(+this.value1, +this.value2, this.currentOperator)
+                this.value2 = ''
+                this.display.push(this.calc(+this.value1, +this.value2, this.currentOperator))
+            }
+            this.currentOperator = btn === '=' ? '' : btn
+        } else {
+            if (this.currentOperator) {
+                this.value2 = this.value2 + btn
+            } else {
+                this.value1 = this.value1 + btn
+            }
+        }
+    }
+}
+
+export default new CalcState()
