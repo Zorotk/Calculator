3cb14bdd79b5cc4e2cde2a88f7f0b8972773bd82
Blazor <zorotkov@rambler.ru>
2021-04-10 22:57:44 +0300

    add display

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import '../styles/toolbar.scss'
 import CalcState from '../store/calcState'
 import { observer } from 'mobx-react-lite'
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 const Calculator = observer(() => {
     return (
         <div className="Calculator">
-            {/* <h2>{CalcState.display.join('')}</h2> */}
+
             <div className={'Calculator__panel'}>
-                <h2> {CalcState.value2 == '' ? CalcState.value1 == '' ? 0 : CalcState.value1 : CalcState.value2}</h2>
+                <div className='Calculator__panel__display'>{CalcState.display}</div>
+                <h2> {CalcState.value2 == ''
+                    ? CalcState.value1 || 0
+                    : CalcState.value2}</h2>
                 <Button variant="success" className={'Calculator__panel__ext'} onClick={() => CalcState.extend()}>расширенный режим</Button>
                 <div >
                     {CalcState.btnValues.map((btn, i) => (
--- src/store/calcState.js
+++ src/store/calcState.js
@@ -5,7 +5,7 @@ class CalcState {
     value1 = ''
     value2 = ''
     currentOperator = ''
-    display = []
+    display = ''
     constructor() {
         makeAutoObservable(this)
     }
@@ -25,8 +25,8 @@ class CalcState {
             case '/': return v1 / v2
             case 'c': return this.value1 = '', this.value2 = '', this.display = []
             case '←': return this.currentOperator
-                ? this.value2 = this.value2.substring(0, this.value2.length - 1)
-                : this.value1 = this.value1.substring(0, this.value1.length - 1)
+                ? this.value2 = this.value2.substring(0, this.value2.length - 1) 
+                : this.value1 = this.value1.substring(0, this.value1.length - 1)  
             case 'x*': return v1 ** v2
             case 'sqrt': return this.value1 = Math.sqrt(v1)
             case 'x2': return this.value1 = v1 * v1
@@ -37,21 +37,25 @@ class CalcState {
         if (btn == 'sqrt' || btn == '←' || btn == 'c' || btn == 'x2') {
             return this.calc(+this.value1, +this.value2, btn)
         }
-        this.display.push(btn)
-
         const isOperator = this.btnValues.slice(11).some(v => v == btn)
         if (isOperator) {
             if (this.value2 !== '' && this.currentOperator) {
                 this.value1 = this.calc(+this.value1, +this.value2, this.currentOperator)
-                this.value2 = ''
-                this.display.push(this.calc(+this.value1, +this.value2, this.currentOperator))
+                this.display += this.value2
+                this.value2 = ''                
             }
             this.currentOperator = btn === '=' ? '' : btn
+            if (this.btnValues.slice(11).join('').includes(this.display[this.display.length - 1])) {
+                this.display = this.display.substring(0, this.display.length - 1)
+            }
+            this.display += btn
+
         } else {
             if (this.currentOperator) {
-                this.value2 = this.value2 + btn
+                this.value2 += btn
             } else {
-                this.value1 = this.value1 + btn
+                this.display += btn
+                this.value1 += btn
             }
         }
     }
