6e1a776d305e7b9e675d7c2ceb0e732502624ac9
Blazor <zorotkov@rambler.ru>
2021-04-11 22:50:43 +0300

    add history

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import '../styles/toolbar.scss'
 import CalcState from '../store/calcState'
 import { observer } from 'mobx-react-lite'
-import { Button } from 'react-bootstrap'
+import { Button, ListGroup, Form } from 'react-bootstrap'
+import calcState from '../store/calcState';
 const Calculator = observer(() => {
+    const [checkbox, setCheckbox] = React.useState(true);
+
     return (
         <div className="Calculator">
-
             <div className={'Calculator__panel'}>
                 <div className='Calculator__panel__display'>{CalcState.display}</div>
                 <h2> {CalcState.value2 == ''
@@ -15,10 +17,21 @@ const Calculator = observer(() => {
                 <Button variant="success" className={'Calculator__panel__ext'} onClick={() => CalcState.extend()}>расширенный режим</Button>
                 <div >
                     {CalcState.btnValues.map((btn, i) => (
-                        <Button variant={"light"} className="Calculator__panel__btn" onClick={() => CalcState.btnHandler(btn)} key={i}>{btn} </Button>
+                        <Button variant={i > 10 ? "outline-success" : "light"} className="Calculator__panel__btn" onClick={() => CalcState.btnHandler(btn)} key={i}>{btn} </Button>
                     ))}
                 </div>
             </div>
+            <ListGroup variant="flush" className='Calculator__history'>
+                <div className='Calculator__history__panel'>
+                    <h5>Журнал</h5>
+                    <Form.Check type="checkbox" onChange={() => setCheckbox(!checkbox)} label="Дата и время" />
+                    <Button onClick={()=>CalcState.deleteHistory()} variant="success">Очистить</Button>
+                </div>
+                {
+                    calcState.history.map((el, i) =>
+                    (<ListGroup.Item action variant="light" className='Calculator__history__item' key={i}>
+                        {checkbox ? <span>Дата:{new Date(el.time).toISOString().slice(0, 10)}  Время:{new Date(el.time).toLocaleTimeString()}</span> : null}   {el.value}</ListGroup.Item>))}
+            </ListGroup>
         </div>
     );
 }
--- src/store/calcState.js
+++ src/store/calcState.js
@@ -1,13 +1,23 @@
 import { makeAutoObservable } from "mobx";
-
 class CalcState {
     btnValues = '1234567890.=-+/*←c'.split('')
     value1 = ''
     value2 = ''
     currentOperator = ''
     display = ''
+    history = []
     constructor() {
         makeAutoObservable(this)
+        this.start()
+    }
+
+    start() {
+        this.history = JSON.parse(localStorage.getItem("calc")) || []
+    }
+
+    deleteHistory() {
+        localStorage.removeItem('calc');
+        this.start()
     }
 
     extend() {
@@ -23,10 +33,10 @@ class CalcState {
             case '-': return v1 - v2
             case '*': return v1 * v2
             case '/': return v1 / v2
-            case 'c': return this.value1 = '', this.value2 = '', this.display = []
+            case 'c': return this.value1 = '', this.value2 = '', this.display = ''
             case '←': return this.currentOperator
-                ? this.value2 = this.value2.substring(0, this.value2.length - 1) 
-                : this.value1 = this.value1.substring(0, this.value1.length - 1)  
+                ? this.value2 = this.value2.substring(0, this.value2.length - 1)
+                : this.value1 = this.value1.substring(0, this.value1.length - 1)
             case 'x*': return v1 ** v2
             case 'sqrt': return this.value1 = Math.sqrt(v1)
             case 'x2': return this.value1 = v1 * v1
@@ -42,14 +52,22 @@ class CalcState {
             if (this.value2 !== '' && this.currentOperator) {
                 this.value1 = this.calc(+this.value1, +this.value2, this.currentOperator)
                 this.display += this.value2
-                this.value2 = ''                
+                this.value2 = ''
             }
             this.currentOperator = btn === '=' ? '' : btn
+            if (btn === '=') {
+                return this.history.unshift({
+                    value: this.display + '=' + this.value1,
+                    time: Date.now()
+                }),
+                    this.value1 = ''
+                    , this.display = ''
+                    , localStorage.setItem('calc', JSON.stringify(this.history));
+            }
             if (this.btnValues.slice(11).join('').includes(this.display[this.display.length - 1])) {
                 this.display = this.display.substring(0, this.display.length - 1)
             }
             this.display += btn
-
         } else {
             if (this.currentOperator) {
                 this.value2 += btn
